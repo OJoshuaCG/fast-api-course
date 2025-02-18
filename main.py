e0eb369f5a18b869c8270e0d418494f410cadbdf
Joshua <ojoshuacg@gmail.com>
2025-02-18 06:23:18 -0600

    feat. Validaciones de datos, 2da parte

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, Path, Query
 from fastapi.responses import HTMLResponse
 from pydantic import BaseModel, Field
 from typing import Optional, List
@@ -68,23 +68,25 @@ def get_movies() -> list[Movie]:
 # Parametros de ruta
 # El valor se incluye en la URL, despues de una diagonal /2
 @app.get("/movies/{id}", tags=["Movies"])
-def get_movie(id: int) -> Movie:
+def get_movie(id: int = Path(gt=0)) -> Movie | dict:
     for movie in movies:
-        if movie["id"] == id:
-            return movie
-    return []
+        if movie.id == id:
+            return movie.model_dump()
+    return {}
 
 
 # Parametros query
 # El valor se define con una clave dentro de la URL, id=1
 @app.get("/movies/", tags=["Movies"])
-def get_movie_by_category(category: str, year: int) -> Movie:
+def get_movie_by_category(
+    category: str = Query(min_length=5, max_length=20)
+) -> Movie | dict:
     # return category
     # return year
     for movie in movies:
-        if movie["category"] == category:
-            return movie
-    return []
+        if movie.category == category:
+            return movie.model_dump()
+    return {}
 
 
 @app.post("/movies", tags=["Movies"])
